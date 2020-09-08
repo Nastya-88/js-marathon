37f16477b7a31d5c82ef9edb3242246523dcf2c9
Анастасия <shishlakovanastya@gmail.com>
2020-09-09 00:21:11 +0300

    HomeWork-9

--- main.js
+++ main.js
@@ -1,66 +1,67 @@
-import Game from './game.js';
-import { pokemons } from './pokemons.js';
 import Pokemon from './pokemon.js';
-import showCountClicked from './showCountClicked.js';
-import { random } from './utils.js';
+import { renderPlayer, btnClicked, addLog } from './utils.js';
 
-const start = new Game();
-start.beginStart();
+class Game {
+    constructor(name) {
+        this.name = name;
+        this.$playground = document.querySelector('.playground');
+        this.$header = document.querySelector('header');
+        this.$pokemons = document.querySelectorAll('.pokemon');
+        this.$startButton = document.createElement('button');
+        this.$resetButton = document.createElement('button');
 
-const pokemonRandom1 = pokemons[random(pokemons.length) - 1];
-const pokemonRandom2 = pokemons[random(pokemons.length) - 1];
+    }
+    getPokemons = async () => {
+        const responce = await fetch('https://reactmarathon-api.netlify.app/api/pokemons?random=true');
+        const body = await responce.json();
+        return body;
+    };
 
-let player1 = new Pokemon({
-    ...pokemonRandom1,
-    selectors: 'player1'
-});
+    start = async () => {
 
-let player2 = new Pokemon({
-    ...pokemonRandom2,
-    selectors: 'player2'
-});
+        this.$playground.style.display = 'none';
 
-export function renderPlayer() {
-
-    const $control = document.querySelector('.control');
-
-    player1.attacks.forEach(item => {
-        // console.log(item);
-        const $btn = document.createElement('button');
-        $btn.classList.add('button');
-        $btn.innerText = item.name;
-        const btnCount = showCountClicked($btn, item.maxCount);
-        $btn.addEventListener('click', () => {
-            console.log('click button', $btn.innerText);
-            btnCount();
-            player1.changeHP(random(item.maxCount), function (count) {
-                generateLog(player1, player2, count);
-            });
-            player2.changeHP(random(item.maxCount), function (count) {
-                generateLog(player2, player1, count);
-            });
-        })
-        $control.appendChild($btn);
-    });
-}
+        this.$startButton.textContent = 'Start Game';
+        this.$startButton.classList.add('button', 'button-start');
+        this.$header.appendChild(this.$startButton);
+        this.$startButton.addEventListener('click', () => {
+            this.playGame();
+            this.$startButton.remove();
+            console.log(this.$startButton);
+        });
 
 
-export function generateLog({ name, hp: { current, total } } = player1, { name: secondName } = player2, count) {
-    const logs = [
-        `${name} вспомнил что-то важное, но неожиданно ${secondName}, не помня себя от испуга, ударил в предплечье врага.  -${count} [${current}/${total}]`,
-        `${name}  поперхнулся, и за это ${secondName} с испугу приложил прямой удар коленом в лоб врага. -${count}[${current}/${total}]`,
-        `${name}  забылся, но в это время наглый ${secondName}, приняв волевое решение, неслышно подойдя сзади, ударил. -${count}[${current}/${total}]`,
-        `${name}  пришел в себя, но неожиданно ${secondName} случайно нанес мощнейший удар. -${count}[${current}/${total}]`,
-        `${name}  поперхнулся, но в это время ${secondName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${count}[${current}/${total}]`,
-        `${name}  удивился, а ${secondName} пошатнувшись влепил подлый удар. -${count}[${current}/${total}]`,
-        `${name}  высморкался, но неожиданно ${secondName} провел дробящий удар. -${count}[${current}/${total}]`,
-        `${name}  пошатнулся, и внезапно наглый ${secondName} беспричинно ударил в ногу противника. -${count}[${current}/${total}]`,
-        `${name}  расстроился, как вдруг, неожиданно ${secondName} случайно влепил стопой в живот соперника. -${count}[${current}/${total}]`,
-        `${name}  пытался что-то сказать, но вдруг, неожиданно ${secondName} со скуки, разбил бровь сопернику. -${count}[${current}/${total}]`
-    ];
+    }
+    playGame = async () => {
 
-    return logs[random(logs.length) - 1];
+        this.$playground.style.display = 'flex';
+        const pokemon1 = await this.getPokemons();
+        const pokemon2 = await this.getPokemons();
+
+        const player1 = new Pokemon({
+            ...pokemon1,
+            selectors: 'player1',
+        });
+
+        const player2 = new Pokemon({
+            ...pokemon2,
+            selectors: 'player2',
+        });
+        console.log(player1);
+        console.log(player2);
+
+        renderPlayer(player1, player2);
+        player1.attacks.forEach(attack => btnClicked(attack, player1, player2));
+
+    };
+    endGame = (log) => {
+        const allButtons = document.querySelectorAll('.control .button');
+        allButtons.forEach($item => $item.remove());
+        log.textContent = '';
+        game.playGame();
+    };
+
 };
 
-
-
+export const game = new Game();
+game.start();
--- utils.js
+++ utils.js
@@ -1,4 +1,87 @@
-export function random(num) {
-    return Math.ceil(Math.random() * num);
+import logs from './logs.js';
+import { game } from './main.js';
+
+
+export const random = (min, max) => Math.ceil(Math.random() * (max - min) + min);
+
+export const addLog = (logStr) => {
+    const $body = document.querySelector('.body');
+    const $fitstLog = document.querySelector('p');
+    const $secondLog = document.createElement('p');
+
+    $secondLog.innerText = logStr;
+    $body.insertBefore($secondLog, $fitstLog);
+
 };
+
+export const generateLog = (character, count, player1, player2) => {
+    const logStr = character === player2 ? logs(character.name, player1.name) : logs(character.name, player2.name);
+    return `${logStr[random(0, logStr.length - 1)]} - ${count} [${character.hp.current}/${character.hp.total}]`;
+};
+
+const disableButton = (button) => {
+    let count = 0;
+
+    return function (clickLimit) {
+        count += 1;
+
+        if (count >= clickLimit) {
+            button.disabled = true
+        };
+        if (count === 0) {
+            alert(`Количество ударов закончилось!`);
+            button.disabled = true;
+        }
+    }
+};
+
+const damage = async () => {
+    const getDamage = async () => {
+        const responce = await fetch('https://reactmarathon-api.netlify.app/api/fight?player1id=25&attackId=1&player2id=1');
+        const body = await responce.json();
+        return body;
+    };
+
+    return await getDamage();
+}
+
+export const btnClicked = (attack, enemy, player) => {
+    const $control = document.querySelector('.control');
+    const $btn = document.createElement('button');
+    $btn.classList.add('button');
+    $btn.innerText = attack.name;
+
+    const btnCount = disableButton($btn);
+    $btn.addEventListener('click', async () => {
+        const { kick } = await damage();
+
+        btnCount(attack.maxCount);
+        player.changeHP(kick.player1, player, enemy);
+        enemy.changeHP(kick.player2, enemy, player);
+
+        if (player.hp.current === 0) {
+            game.endGame();
+        } else if (enemy.hp.current === 0) {
+            game.endGame();
+        };
+
+
+    })
+    $control.appendChild($btn);
+}
+
+export const renderPlayer = (player1, player2) => {
+    player1.renderHP();
+    player2.renderHP();
+
+    const $namePl1 = document.querySelector('#name-player1');
+    $namePl1.innerText = player1.name;
+    const $namePl2 = document.querySelector('#name-player2');
+    $namePl2.innerText = player2.name;
+
+    const $imgPl1 = document.querySelector('.player1 > img');
+    $imgPl1.setAttribute('src', player1.img)
+    const $imgPl2 = document.querySelector('.player2 > img');
+    $imgPl2.setAttribute('src', player2.img)
+}
 
